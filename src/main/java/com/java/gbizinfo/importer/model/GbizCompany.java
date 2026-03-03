95b25a75e8975b63c62f7ec3b078190fc3c74593
Mihaela Catan <mihaelacatan7@gmail.com>
2026-03-03 10:33:01 +0200

    feat: Create company model

---
+++ src/main/java/com/java/gbizinfo/importer/model/GbizCompany.java
@@ -0,0 +1,89 @@
+package com.java.gbizinfo.importer.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Setter
+@Getter
+@NoArgsConstructor
+public class GbizCompany {
+
+    @JsonProperty("corporate_number")
+    private String corporateNumber;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("kana")
+    private String kana;
+
+    @JsonProperty("name_en")
+    private String nameEn;
+
+    @JsonProperty("postal_code")
+    private String postalCode;
+
+    @JsonProperty("location")
+    private String location;
+
+    @JsonProperty("process")
+    private String process;
+
+    @JsonProperty("aggregated_year")
+    private String aggregatedYear;
+
+    @JsonProperty("status")
+    private String status;
+
+    @JsonProperty("close_date")
+    private String closeDate;
+
+    @JsonProperty("close_cause")
+    private String closeCause;
+
+    @JsonProperty("kind")
+    private String kind;
+
+    @JsonProperty("representative_name")
+    private String representativeName;
+
+    @JsonProperty("capital_stock")
+    private Long capitalStock;
+
+    @JsonProperty("employee_number")
+    private Integer employeeNumber;
+
+    @JsonProperty("company_size_male")
+    private Integer companySizeMale;
+
+    @JsonProperty("company_size_female")
+    private Integer companySizeFemale;
+
+    @JsonProperty("business_summary")
+    private String businessSummary;
+
+    @JsonProperty("company_url")
+    private String companyUrl;
+
+    @JsonProperty("founding_year")
+    private Integer foundingYear;
+
+    @JsonProperty("date_of_establishment")
+    private String dateOfEstablishment;
+
+    @JsonProperty("qualification_grade")
+    private String qualificationGrade;
+
+    @JsonProperty("update_date")
+    private String updateDate;
+
+    @JsonProperty("industry")
+    private List<String> industry;
+
+    @JsonProperty("business_items")
+    private List<String> businessItems;
+}

e8e7c80201ce8a2b480ae90b4bfe71163efeee64
Mihaela Catan <mihaelacatan7@gmail.com>
2026-03-03 10:18:31 +0200

    feat: Initialize project

---
+++ src/main/java/com/java/gbizinfo/importer/GBizInfoJsonImporterApplication.java
@@ -0,0 +1,13 @@
+package com.java.gbizinfo.importer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GBizInfoJsonImporterApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(GBizInfoJsonImporterApplication.class, args);
+    }
+
+}

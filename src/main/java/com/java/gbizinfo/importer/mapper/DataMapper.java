ea65e850f0d82780dc89a99536f0934fa7b2b805
Mihaela Catan <mihaelacatan7@gmail.com>
2026-03-03 10:29:50 +0200

    feat: Create data mapper

---
+++ src/main/java/com/java/gbizinfo/importer/mapper/DataMapper.java
@@ -0,0 +1,13 @@
+package com.java.gbizinfo.importer.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface DataMapper {
+    int getTimesCheckpointed(@Param("entryName") String entryName);
+
+    int copy(@Param("entryName") String entryName);
+
+    void insertCheckpoint(@Param("entryName") String entryName);
+}

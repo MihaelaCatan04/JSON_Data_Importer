85eaef7814a8aa43f8554be43252f81d19b7b7d9
Mihaela Catan <mihaelacatan7@gmail.com>
2026-03-04 17:32:37 +0200

    feat: Implement scheduling and remove controller

--- src/main/java/com/java/gbizinfo/importer/GBizInfoJsonImporterApplication.java
+++ src/main/java/com/java/gbizinfo/importer/GBizInfoJsonImporterApplication.java
@@ -2,7 +2,9 @@ package com.java.gbizinfo.importer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class GBizInfoJsonImporterApplication {
 

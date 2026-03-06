0bce5f5d6e67c0d7cf793d723d149f7802eb0730
Mihaela Catan <mihaelacatan7@gmail.com>
2026-03-06 15:46:21 +0200

    refactor: Split sql scripts into migrations

---
+++ src/main/resources/migrations/migration1/CreateSequences.sql
@@ -0,0 +1,4 @@
+CREATE SEQUENCE company_id_seq;
+CREATE SEQUENCE patent_id_seq;
+CREATE SEQUENCE workplace_info_id_seq;
+CREATE SEQUENCE finance_id_seq;
---
+++ src/main/resources/migrations/migration3/WorkplaceInfoTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE workplace_info
+(
+    workplace_info_id BIGINT DEFAULT nextval('workplace_info_id_seq') PRIMARY KEY,
+    company_id        BIGINT,
+    FOREIGN KEY (company_id) REFERENCES company (company_id)
+);

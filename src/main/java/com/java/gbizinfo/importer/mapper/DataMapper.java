67a7d1a5fdd2d54dafe7602e48e6380ea472c32e
Mihaela Catan <mihaelacatan7@gmail.com>
2026-03-06 15:49:23 +0200

    fix: Modify Data Mapper according to the new models

--- src/main/java/com/java/gbizinfo/importer/mapper/DataMapper.java
+++ src/main/java/com/java/gbizinfo/importer/mapper/DataMapper.java
@@ -5,11 +5,11 @@ import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface DataMapper {
-    int getTimesCheckpointed(@Param("entryName") String entryName);
+    int getTimesCheckpointed(@Param("runId") String runId, @Param("entryName") String entryName);
+
+    void insertCheckpoint(@Param("runId") String runId, @Param("entryName") String entryName);
 
     int copy(@Param("entryName") String entryName);
-
-    void insertCheckpoint(@Param("entryName") String entryName);
 
     long getMaxPatentId();
 

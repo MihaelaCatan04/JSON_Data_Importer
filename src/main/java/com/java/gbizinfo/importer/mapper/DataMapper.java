25fa3ad91b55c947943be8aa85ea2e65a4976f89
Mihaela Catan <mihaelacatan7@gmail.com>
2026-03-18 15:12:19 +0200

    fix: Alter mapper logic for usage

--- src/main/java/com/java/gbizinfo/importer/mapper/DataMapper.java
+++ src/main/java/com/java/gbizinfo/importer/mapper/DataMapper.java
@@ -5,17 +5,12 @@ import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface DataMapper {
-    int getTimesCheckpointed(@Param("runId") String runId, @Param("entryName") String entryName);
 
-    void insertCheckpoint(@Param("runId") String runId, @Param("entryName") String entryName);
+    int getTimesCheckpointed(@Param("runId") String runId,
+                             @Param("entryName") String entryName);
+
+    void insertCheckpoint(@Param("runId") String runId,
+                          @Param("entryName") String entryName);
 
     int copy(@Param("entryName") String entryName);
-
-    long getMaxPatentId();
-
-    long getMaxWorkplaceInfoId();
-
-    long getMaxFinanceId();
-
-    long getMaxCompanyId();
 }
